7e7f36e18e8b263f0f74f6fbb2bb1bc5ffae4972
Shepherd <nothing@nothing.com>
2025-05-03 17:07:36 +0300

    Second point

--- backup.sh
+++ backup.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
 
+title=""
+
 while IFS= read -r line;
 do
 	for path in $(find $HOME -type d -name $(echo $line | awk '{print $1}')); do
+		
+		if [[ "$title" != $(echo "$line" | awk '{print $1}') ]]; then
+			title=$(echo $line | awk '{print $1}')
+			echo $title
+		fi
+
 		if [[ $(echo $line | awk '{print $2}') == *"."* ]]; then
 			find $path -type f -name "$(echo $line | grep $line | awk '{print $2}')" | xargs -I {} echo "-> {}"
-			echo "This is $line"
 		else
-			find $path -type d -name "$(echo $line | awk '{print $2}')" | xargs -I {} echo "-> {}"
+			source=$(find $path -type d -name "$(echo $line | awk '{print $2}')" )
+			echo $source | xargs -I {} echo -e "\e[32m->\e[0m {}" 
 		fi
 	done
 done < "txt.txt"
