307b11d5c5503b02178e4a18b6a90505ecf9288b
Shepherd <nothing@nothing.com>
2025-05-03 20:25:54 +0300

    Fuck this shit I'm out!

--- backup.sh
+++ backup.sh
@@ -6,6 +6,8 @@ while IFS= read -r line;
 do
 	for path in $(find $HOME -type d -name $(echo $line | awk '{print $1}')); do
 		
+		destFolder=$(echo $line | awk '{print $3}')
+
 		if [[ "$title" != $(echo "$line" | awk '{print $1}') ]]; then
 			title=$(echo $line | awk '{print $1}')
 			echo $title
@@ -16,11 +18,17 @@ do
 		else
 			source=$(find $path -type d -name "$(echo $line | awk '{print $2}')" )
 			echo $source | xargs -I {} echo -e "\e[32m->\e[0m {}"
-			
+			#echo "$destFolder"
 			#mkdir -p $destFolder
+			
+			if [[ ! -f $destFolder$(echo $line | awk '{print $2}') ]]; then
+				mkdir -p $destFolder$(echo $line | awk '{print $2}')
+			fi
 
-#			cp -r $source $destFolder
+			cp -r $source $destFolder
 			echo $destFolder
+			#echo "$source" "$destFolder/"
+			 
 		fi
 	done
 done < "txt.txt"
